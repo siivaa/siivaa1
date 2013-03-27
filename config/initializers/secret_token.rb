985349d77c89965297fb4fa5f1edf7c004d4d146
siivaa <auis_su@yahoo.com>
2013-03-27 16:40:06 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siivaa::Application.config.secret_token = '48c773df13da86b9c2d150bc1785a5b527f207f4e20ab6042dbaeb49d845120384db507026ec2fd081809b27d4d2a6aeaf028f45cd9302677035ec96e157aa54'
